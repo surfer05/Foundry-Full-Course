33be02391ab09eac77e3a63daba71a70d45f43bb
Surfer05 <rahul05alok@gmail.com>
2023-07-24 22:39:26 +0530

    Helper Config done

--- foundry-fund-me-f23/test/FundeMeTest.t.sol
+++ foundry-fund-me-f23/test/FundeMeTest.t.sol
@@ -3,15 +3,27 @@ pragma solidity ^0.8.18;
 
 import {Test , console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
-    function testDemo() public {
+    function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(),5e18);
     }
+
+    function testOwner() public{
+        assertEq(fundMe.i_owner(),address(this));
+    }
+
+    function testPriceFeedVersionIsAccurate() public{
+        uint version = fundMe.getVersion();
+        assertEq(version,4);
+    }
+
 }

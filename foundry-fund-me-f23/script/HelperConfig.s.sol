1a25d83e89bcccc274adfc759e9d303b77d917a7
Surfer05 <rahul05alok@gmail.com>
2023-07-30 14:11:29 +0530

    Phase-2 2023

--- foundry-fund-me-f23/script/HelperConfig.s.sol
+++ foundry-fund-me-f23/script/HelperConfig.s.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {Script} from "forge-std/Script.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script {
     // If we are on a local anvil chain, we deploy mocks,
     // Otherwise, grab the existing address from the live network
 
@@ -12,11 +13,14 @@ contract HelperConfig {
     }
     NetworkConfig public activeNetworkConfig;
 
-    constructor()  {
-        if(block.chainid == 11155111) activeNetworkConfig = getSepoliaEthConfig();
-        else activeNetworkConfig = getAnvilEthConfig();
-    }
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
+    constructor() {
+        if (block.chainid == 11155111)
+            activeNetworkConfig = getSepoliaEthConfig();
+        else activeNetworkConfig = getOrCreateAnvilEthConfig();
+    }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory sepoliaConfig = NetworkConfig({
@@ -26,7 +30,24 @@ contract HelperConfig {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory){
-        
+    function getOrCreateAnvilEthConfig()
+        public
+        returns (NetworkConfig memory anvilNetworkConfig)
+    {
+        // Check to see if we set an active network config
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+        // emit HelperConfig__CreatedMockPriceFeed(address(mockPriceFeed));
+
+        anvilNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+
+        return anvilNetworkConfig;
     }
 }
--- foundry-fund-me-f23/test/FundeMeTest.t.sol
+++ foundry-fund-me-f23/test/FundeMeTest.t.sol
@@ -17,13 +17,18 @@ contract FundMeTest is Test {
         assertEq(fundMe.MINIMUM_USD(),5e18);
     }
 
-    function testOwner() public{
-        assertEq(fundMe.i_owner(),address(this));
-    }
+    // function testOwner() public{
+    //     assertEq(fundMe.i_owner(),address(this));
+    // }
 
     function testPriceFeedVersionIsAccurate() public{
         uint version = fundMe.getVersion();
         assertEq(version,4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // Next line must fail for the test to be successful
+        fundMe.fund();  // sending 0 value, thus it will fail and therefore test will pass
     }
 
 }

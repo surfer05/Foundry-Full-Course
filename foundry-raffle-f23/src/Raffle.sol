7dc144d4cd71cc511fd4f0ce99ee34a6e8292e04
Surfer05 <rahul05alok@gmail.com>
2023-08-05 15:32:31 +0530

    Using VRF

--- foundry-raffle-f23/src/Raffle.sol
+++ foundry-raffle-f23/src/Raffle.sol
@@ -33,14 +33,32 @@ contract Raffle {
     error Raffle__NotEnoughEthSent();
 
     /** State variables */
+    uint256 private constant REQUEST_CONFIRMATIONS = 3;
+    uint private constant NUM_WORDS = 1;
+
     uint private immutable i_entranceFee; // do we want to update the entrance fee, not really, thus make it immutable
+    uint private immutable i_interval;
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    uint private s_lastTimestamp;
+    // @dev duration of the lottery in seconds
     address payable[] private s_players;
+
 
     /** Events */
     event EnteredRaffle(address indexed player);
 
-    constructor(uint entranceFee) {
+    constructor(uint entranceFee, uint interval, address vrfCoordinator, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = vrfCoordinator;
+        s_lastTimestamp = block.timestamp;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -48,10 +66,27 @@ contract Raffle {
             revert Raffle__NotEnoughEthSent();
         }
         s_players.push(payable(msg.sender));
+
+        // Makes migration easier
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    // Get a random number
+    // Use the random number to pick a player
+    // Be automatically called
+    function pickWinner() public {
+        if(block.timestamp - s_lastTimestamp < i_interval){
+            revert();
+        }
+
+        uint requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );1
+    }
 
     /** Getter functions */
 
